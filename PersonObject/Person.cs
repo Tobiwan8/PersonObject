6e3f8f701eb6e68e19ff536bffc5fb37f65bae8e
Tobias <tobiaslarsen93@gmail.com>
2022-08-24 08:51:31 +0200

    Add project files.

---
+++ PersonObject/Person.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonObject
+{
+    internal class Person
+    {
+        private bool isUpper;
+        private bool isLower;
+        private bool isNumber;
+        private string email = "";
+        private string password = "";
+        public string Name { get; set; }
+        public DateTime DoB { get; set; }
+        public int Age { get { return GetAge(); } }
+        public int GetAge()
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - DoB.Year;
+            if (today < new DateTime(today.Year, DoB.Month, DoB.Day)) age--;
+            return age;
+        }
+        public string Email 
+        {
+            get { return email; }
+            set
+            {
+                if (value.Contains("@") && value.Contains("."))
+                {
+                    email = value;
+                }
+                else
+                {
+                    Console.WriteLine(value + " Email must contain @ and .");
+                    email = null;
+                }
+            } 
+        }
+        public string Password 
+        {
+            get { return password; }
+            set
+            {
+                if(value.Length < 6)
+                {
+                    Console.WriteLine("Must be at least 6 characters");
+                    password = null;
+                }
+
+                foreach(char c in value)
+                {
+                    if (char.IsUpper(c)) { isUpper = true; }
+                    if (char.IsLower(c)) { isLower = true; }
+                    if (char.IsDigit(c)) { isNumber = true; }
+                }
+
+                if(isUpper == true && isLower == true && isNumber == true)
+                {
+                    password = value;
+                }
+                else
+                {
+                    Console.WriteLine(value + " Password must contain small and big letters and numbers");
+                    password = null;
+                }
+            }
+        }
+    }
+}
---
+++ PersonObject/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.Name = "Konrad";
+            person1.DoB = new DateTime(1990, 3, 2);
+            person1.Email = "konrad@email.com";
+            person1.Password = "HejKonrad1990";
+
+            Person person2 = new Person();
+            person2.Name = "Connie";
+            person2.DoB = new DateTime(1991, 9, 16);
+            person2.Email = "connie@email,com";
+            person2.Password = "HejConnie1991";
+
+            Person person3 = new Person();
+            person3.Name = "Bent";
+            person3.DoB = new DateTime(1971, 5, 27);
+            person3.Email = "bent@email.com";
+            person3.Password = "hejbent1971";
+
+            Person person4 = new Person();
+            person4.Name = "Bitten";
+            person4.DoB = new DateTime(1974, 12, 24);
+            person4.Email = "bitten@email.com";
+            person4.Password = "HejBitten";
+
+            Console.WriteLine("\n" + person1.Name);
+            Console.WriteLine(person1.Age);
+            Console.WriteLine(person1.Email);
+            Console.WriteLine(person1.Password);
+
+            Console.ReadKey();
+        }
+    }
+}
